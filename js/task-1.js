681c2d2082feffd6457f6a49dee9828a9761367a
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-04-30 23:39:09 +0300

    123

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+
+
+categoriesItems.forEach(item => {
+    const categoryTitle = item.querySelector('h2').textContent;
+    const categoryItems = item.querySelectorAll('ul > li');
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryItems.length}`);
+});
