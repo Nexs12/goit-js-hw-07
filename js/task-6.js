d1223551ba9891ea1b63fe38d7a19f752395f486
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-01 23:17:46 +0300

    hw7

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+  const controls = document.getElementById('controls');
+  const input = controls.querySelector('input');
+  const createBtn = controls.querySelector('[data-create]');
+  const destroyBtn = controls.querySelector('[data-destroy]');
+  const boxesContainer = document.getElementById('boxes');
+
+  createBtn.addEventListener('click', () => {
+    const amount = parseInt(input.value);
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      input.value = '';
+    }
+  });
+
+   function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
+
+  destroyBtn.addEventListener('click', () => {
+    destroyBoxes();
+  });
+
+  function createBoxes(amount) {
+    destroyBoxes(); 
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+      size += 10;
+    }
+  }
+
+ 
