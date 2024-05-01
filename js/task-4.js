45ed7cf75d5e31dafa7718868d147d9a1da7ea98
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-01 18:57:52 +0300

    task-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+'use strict'
 
+const registerForm = document.querySelector(".login-form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  
+  if (login === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+  
+    const userData = {
+        login,
+        password
+    };
+
+    console.log(userData);
+  form.reset();
+}
